24f12e2229f1e5e67ece662421da949aac729448
guddndhk <guddndhk@naver.com>
2022-02-17 03:43:04 +0900

    22/02/17 python print() study

---
+++ chapter02_01.py
@@ -0,0 +1,62 @@
+# Print 사용법
+# 기본 출력
+print('Python Start!')
+print("Python Start!")
+print('''Python Start!''')
+print("""Python Start!""")
+
+print()  # 개행 역할가능
+
+# separator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '3333', '1111', sep='-')
+print('python', 'google.com', sep='@')
+
+print()
+
+# end 옵션
+print('welcome to', end=' ')
+print('IT News', end=' ')
+print('Web Site')
+print()
+# file 옵션
+import sys
+
+print('Learn Python', file=sys.stdout)
+print()
+
+# format 사용 (d : 3, s : 'python, f : 3.148592)
+print('%s %s' % ('one', 'two'))
+print('{} {}'.format('one', 'two'))
+print('{1} {0}'.format('one', 'two'))
+print()
+
+# %s
+print('%10s' % ('nice'))
+print('{:>10}'.format('nice'))
+
+print('%-10s' % ('nice'))
+print('{:10}'.format('nice'))
+
+print('{:_>10}'.format('nice'))
+print('{:^10}'.format('nice'))
+
+print('%.5s' % ('nice'))
+print('%.5s' % ('pythonstudy'))
+print('{:10.5}'.format('pythonstudy'))
+
+# %d
+print('%d %d' % (1, 2))
+print('{} {}'.format(1, 2))
+
+print('%4d' % (42))
+print('{:4d}'.format(42))
+
+# %f
+print('%f' % (3.1485912312321))
+print('{:f}'.format(3.1485912312321))
+
+print('%06.2f' % (3.141592653589793))
+print('{:06.2f}'.format(3.141592653589793))
+
+
