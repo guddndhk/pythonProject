a1caa311a4f4952fee20b6d68d010e3347284bae
guddndhk <guddndhk@naver.com>
2022-02-18 06:17:49 +0900

    22/02/18 python print() study

--- chapter02_02.py
+++ chapter02_02.py
@@ -9,3 +9,68 @@ print(type(n))
 
 # 동시 선언
 x = y = z = 700
+print(x, y, z)
+
+# 선언
+var = 75
+
+# 재선언
+var = "Change Value"
+
+# 출력
+print(var)
+print(type(var))
+
+# Object References
+# 변수 값 할당 상태
+# 1. 타입에 맞는 오브젝트 생성
+# 2. 값 생성
+# 3. 콘솔 출력
+
+# ex
+print(300)
+print(int(300))
+
+# ex2
+n = 777
+
+print(n, type(n))
+print()
+
+m = n
+
+print(m, n)
+print(type(m), type(n))
+print()
+
+m = 400
+
+print(m, n)
+print(type(m), type(n))
+print()
+
+# id(identity)확인 : 객체의 고유값 확인
+m = 800
+n = 655
+
+print(id(m))
+print(id(n))
+print(id(m) == id(n))
+print()
+
+m = 800
+n = 800
+
+print(id(m))
+print(id(n))
+print(id(m) == id(n))
+print()
+
+# 다양한 변수 선언
+# Camel Case : 메소드
+# Pascal Case : 클래스 .. 자바와 같다
+# Snake Case : 파이썬에서 잘 사용함 변수선언 이라던지..
+
+student_grade = 3
+
+# 자바와 마찬 가지로 예약어는 변수 선언 안된다.
